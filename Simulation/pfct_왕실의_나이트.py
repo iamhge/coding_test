7fe9b2fb42f0ed7f2cfd4f86fb1462e035e61b03
iamhge <fine632@gmail.com>
2021-10-06 02:37:56 +0900

    왕실의 나이트

--- Simulation/pfct_왕실의_나이트.py
+++ Simulation/pfct_왕실의_나이트.py
@@ -17,3 +17,24 @@ for i in range(len(dx)):
         count += 1
 
 print(count)
+
+# 책 풀이 방식
+'''
+import sys
+
+xy = sys.stdin.readline().rstrip()
+x = ord(xy[0]) - ord('a') + 1
+y = int(xy[1])
+
+steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+count = 0
+
+for step in steps:
+    nx = x + step[0]
+    ny = y + step[1]
+    
+    if 1 <= nx <= 8 and 1 <= ny <= 8:
+        count += 1
+
+print(count)
+'''
