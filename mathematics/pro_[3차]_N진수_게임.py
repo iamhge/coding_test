e3d1011393f877eb35c1f3cdd3da7f31d691f99f
iamhge <fine632@gmail.com>
2022-01-29 13:03:40 +0900

    [3차] N진수 게임

---
+++ mathematics/pro_[3차]_N진수_게임.py
@@ -0,0 +1,27 @@
+# N진수 게임
+nums = [str(i) for i in range(10)]
+nums.extend([chr(65+i) for i in range(6)])
+
+def nNumeral(n, num):
+    result = ''
+    while num > n:
+        result += nums[num % n]
+        num //= n
+    result += nums[num % n]
+    if n <= num:
+        result += nums[num // n]
+    return result[::-1]
+
+def solution(n, t, m, p):
+    answer = ''
+    now = 0
+    total = ''
+    
+    while len(total) < m*(t-1) + p:
+        total += nNumeral(n, now)
+        now += 1
+    
+    for i in range(p-1, m*(t-1)+p, m):
+        answer += total[i]
+    
+    return answer
