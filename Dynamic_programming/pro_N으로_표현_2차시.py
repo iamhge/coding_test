1082217d9fe893ee98d261926f3ae6c1e8685e0b
iamhge <fine632@gmail.com>
2022-06-29 21:49:48 -0400

    N으로 표현 2차시

---
+++ Dynamic_programming/pro_N으로_표현_2차시.py
@@ -0,0 +1,37 @@
+# N으로 표현
+# N으로 표현
+# 2차시
+'''
+- 또 못 풀어서 답 보고 풂
+<풀이 방법>
+  - N이 n개 사용됐을 경우를 모두 구한다. (n < 9)
+    N이 n개 사용된 숫자
+    = N이 1개 사용된 숫자 + N이 n-1개 사용된 숫자
+    = N이 2개 사용된 숫자 + N이 n-2개 사용된 숫자
+    = N이 3개 사용된 숫자 + N이 n-3개 사용된 숫자
+    ...
+    = N이 8개 사용된 숫자 + N이 n-8개 사용된 숫자
+'''
+def solution(N, number):
+    answer = -1
+    dp = [[]]
+    
+    for i in range(1, 9):
+        cases = set()
+        cases.add(int(str(N)*i))
+        
+        for j in range(i):
+            for op1 in dp[j]:
+                for op2 in dp[i-j]:
+                    cases.add(op1 + op2)
+                    cases.add(op1 * op2)
+                    cases.add(op1 - op2)
+                    if op2 != 0:
+                        if op1 % op2 == 0:
+                            cases.add(op1 // op2)
+        if number in cases:
+            answer = i
+            break
+        dp.append(cases)
+            
+    return answer
