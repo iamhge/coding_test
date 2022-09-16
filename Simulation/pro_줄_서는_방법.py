d9f746f9cdfed629f67a85cd20db6ac57e10c9ea
iamhge <fine632@gmail.com>
2022-09-16 17:41:08 +0900

    줄 서는 방법

--- Simulation/pro_줄_서는_방법.py
+++ Simulation/pro_줄_서는_방법.py
@@ -1,4 +1,45 @@
 # 줄 서는 방법
+# 2차시
+'''
+<풀이 방법>
+  - a_1 = (k + (n-1)) // n
+    a_2 ~ a_n 의 개수 = (n-1)!
+  - a_1을 구한 후, a_2 ~ a_n 에서의 a_1을 다시 구하는 것을 반복한다.
+    a_1을 구한 후 다음 k = (k + (factorial(n)) // (n) - 1) % ((factorial(n)) // (n) ) + 1
+'''
+fact = []
+
+def factorial(n):
+    global fact
+    if fact[n] != 1 or n <= 1:
+        return fact[n]
+    
+    for i in range(1, n+1):
+        fact[i] = fact[i-1] * i
+    return fact[n]
+
+def solution(n, k):
+    global fact 
+    fact = [1] * (n+1)
+    
+    numbers = [i for i in range( n+1)]
+    answer = []
+    remain = n
+    
+    for _ in range(n):
+        divide = factorial(remain) // remain
+        
+        nextPerson = (k + divide-1) // divide
+        answer.append(numbers.pop(nextPerson))
+        
+        k = (k + divide - 1) % divide + 1
+        remain -= 1
+        
+    
+    return answer
+
+# 1차시
+# 오답 2) 런타임 에러
 '''
 문제 풀이 실패
   사유) 런타임 에러를 못 고침
@@ -25,7 +66,7 @@ def solution(n, k):
         k = k % iFac
     return answer
 
-# 시간 초과
+# 오답 1) 시간 초과
 '''
 <풀이 방법>
   - 모든 경우의 수를 구한 후 정렬한다.
